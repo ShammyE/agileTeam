8ccba0f5a859d4c8a4c9b419183458a228775c4e
Maurice-Hayward <mauricehayward1@gmail.com>
2017-07-10 15:54:36 -0400

    yo install angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name agileAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the agileAppApp
+ */
+angular.module('agileAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name agileAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the agileAppApp
+ */
+angular.module('agileAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
