5e8c13abf5a014faac5320f91230d6adb2d931da
Shannon Enders <shannon.enders@ferguson.com>
2017-07-11 16:30:30 -0400

    Changes to the buttons

--- app/scripts/component/associates.component.js
+++ app/scripts/component/associates.component.js
@@ -7,9 +7,42 @@ angular.module("agileAppApp").component("associates", {
   					then(function (results){
   						model.associates = results.data}
   		)
+
+
+  		model.orderProp = 'name';
+  		model.activeButton = {name:1,aaNum: 0,'team.current':0 ,title:0,teamPos: 0,manager:0} //["Name", "AAnum", "Team" ,'Title', 'Team Position', 'Manager'] Header array
+  		console.log(model.activeButton)
+
+
+
+  		model.sortFunc = function (prop) {
+  			model.orderProp = prop;
+
+  			for(let key in model.activeButton){
+  				
+
+  				if(key === prop){
+  					console.log(key)
+  				console.log(prop)
+  					console.log("we made it")
+  					model.activeButton[key] = 1
+  				}
+  				else{
+  					model.activeButton[prop] = 0
+  				}
+
+  				
+  			}
+
+  			console.log(model.activeButton)
+
+
+  		}
+
   	}
 
   ]
 
 
 });
+ 
