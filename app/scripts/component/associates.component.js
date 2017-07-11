607f51f722d31daaa4e9929ce99c26d42576e78f
Maurice-Hayward <mauricehayward1@gmail.com>
2017-07-11 17:06:43 -0400

    change active button list

--- app/scripts/component/associates.component.js
+++ app/scripts/component/associates.component.js
@@ -1,48 +1,39 @@
 angular.module("agileAppApp").component("associates", {
   templateUrl: "views/associates.component.html",
   controllerAs: "model",
-  controller: ["associateData", function(associateData) {
-  		let model = this;
-  		associateData.getAllAssociates().
-  					then(function (results){
-  						model.associates = results.data}
-  		)
+  controller: [
+    "associateData",
+    function(associateData) {
+      let model = this;
 
+      model.orderProp = "name";
+      model.activeButton = {
+        name: 1,
+        aaNum: 0,
+        "team.current": 0,
+        title: 0,
+        teamPos: 0,
+        manager: 0
+      }; //["Name", "AAnum", "Team" ,'Title', 'Team Position', 'Manager'] Header array
 
-  		model.orderProp = 'name';
-  		model.activeButton = {name:1,aaNum: 0,'team.current':0 ,title:0,teamPos: 0,manager:0} //["Name", "AAnum", "Team" ,'Title', 'Team Position', 'Manager'] Header array
-  		console.log(model.activeButton)
+      associateData.getAllAssociates().then(function(results) {
+        model.associates = results.data;
+      });
 
+      model.sortFunc = function(prop) {
+        model.orderProp = prop;
+        model.toggleButtonPrimary(prop);
+      };
 
-
-  		model.sortFunc = function (prop) {
-  			model.orderProp = prop;
-
-  			for(let key in model.activeButton){
-  				
-
-  				if(key === prop){
-  					console.log(key)
-  				console.log(prop)
-  					console.log("we made it")
-  					model.activeButton[key] = 1
-  				}
-  				else{
-  					model.activeButton[prop] = 0
-  				}
-
-  				
-  			}
-
-  			console.log(model.activeButton)
-
-
-  		}
-
-  	}
-
+      model.toggleButtonPrimary = function(prop) {
+        for (let key in model.activeButton) {
+          if (key === prop) {
+            model.activeButton[key] = 1;
+          } else {
+            model.activeButton[key] = 0;
+          }
+        }
+      };
+    }
   ]
-
-
 });
- 
